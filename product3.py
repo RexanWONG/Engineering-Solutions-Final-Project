981ed457f4be6b7b088a723a02e85a309d96c698
RexanWONG <96183717+RexanWONG@users.noreply.github.com>
2023-05-17 09:45:18 +0800

    Change product3

--- product3.py
+++ product3.py
@@ -5,33 +5,25 @@ adjusts smart bulbs to turn on/off based on location
 
 from microbit import *
 
-distanceThresholdClosest = 200
-distanceThresholdClose = 400
-distanceThresholdFar = 600
+distanceThreshold = 260
 
 ultrasonicPin = pin0
 firstBulb = pin1
 secondBulb = pin2
-thirdBulb = pin3
 
 while True:
     distance = ultrasonicPin.read_analog()
 
-    if distance < distanceThresholdClosest:
+    display.scroll(distance)
+
+    if distance < distanceThreshold:
         firstBulb.write_digital(1)
         secondBulb.write_digital(1)
-        thirdBulb.write_digital(1)
-    elif distance < distanceThresholdClose:
-        firstBulb.write_digital(1)
-        secondBulb.write_digital(1)
-        thirdBulb.write_digital(0)
-    elif distance < distanceThresholdFar:
-        firstBulb.write_digital(1)
+    elif distance > distanceThreshold:
+        firstBulb.write_digital(0)
         secondBulb.write_digital(0)
-        thirdBulb.write_digital(0)
     else:
         firstBulb.write_digital(0)
         secondBulb.write_digital(0)
-        thirdBulb.write_digital(0)
 
-    sleep(100)
+    sleep(1000)
